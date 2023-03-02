690ff95ccfd48df780324984bde40bdddd095d61
Robertspoon <robertthemcjug@gmail.com>
2023-03-02 10:00:26 -0400

    added more buttons

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    void QuitGame ()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Quit!!!!!");
+        Application.Quit();
     }
 }
